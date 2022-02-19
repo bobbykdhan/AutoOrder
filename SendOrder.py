89413870432d79d10890cc60e602b030f0652508
bobby <bkd7911@g.rit.edu>
2022-02-19 14:47:20 -0500

    Fixed the submenu scraping and started to refactor

--- SendOrder.py
+++ SendOrder.py
@@ -10,7 +10,6 @@ from selenium.webdriver.support.wait import WebDriverWait
 from Login import signIn
 
 
-
 @dataclass
 class Item:
     """Class for keeping track of an item in store inventory."""
@@ -18,55 +17,34 @@ class Item:
     price: float
     cartPointer: WebElement
 
-PATH = "/Users/bobby/Documents/Projects/AutoOrder/chromedriver"
 
+PATH = "/Users/bobby/Documents/Projects/AutoOrder/chromedriver"
 
 service = Service(PATH)
 options = webdriver.ChromeOptions()
 driver = webdriver.Chrome(service=service, options=options)
 
-driver.get("https://ondemand.rit.edu/menu/dc9df36d-8a64-42cf-b7c1-fa041f5f3cfd/2195/5519/6")
+driver.get("https://ondemand.rit.edu/menu/dc9df36d-8a64-42cf-b7c1-fa041f5f3cfd/2163/5621/5")
 input("waiting for site to load")
 
 itemElements = driver.find_elements(By.CLASS_NAME, "top-container")
-itemFeatures = []
-for item in itemElements:
-    itemFeatures.append(item.find_element(By.CLASS_NAME, "title-hover"))
-
-# //div[@id='parent']/div/div/div[6]/div/div/a/div
-
 items = []
-test = []
-prices = []
-
 count = 0
-for itemFeature in itemFeatures:
+for item in itemElements:
 
+    bottomContainer = item.find_element(By.CLASS_NAME, "bottom-container")
+    name = item.find_element(By.CLASS_NAME, "title-hover").text
+    print("added: " + name)
 
-    idkTest = itemElements[itemFeatures.index(itemFeature)].find_element(By.CLASS_NAME, "bottom-container")
-
-
-    print("added: " + itemFeature.text)
-    try:
-        prices.append(idkTest.find_element(By.CLASS_NAME, "amount"))
-    except:
-        print("Didn't find it:" + itemFeature.text)
-
-
-
-    items.append(Item(itemFeature.text,idkTest.find_element(By.CLASS_NAME, "amount").text,idkTest.find_element(By.CLASS_NAME, "add-to-cart-text")))
-
-
+    items.append(Item(name, bottomContainer.find_element(By.CLASS_NAME, "amount").text,
+                      bottomContainer.find_element(By.CLASS_NAME, "add-to-cart-text")))
 
     try:
-        # exec(input("Type something"))
         items[count].cartPointer.click()
         driver.find_element(By.CLASS_NAME, "BottomContainer").find_element(By.CLASS_NAME, 'add-to-cart-button ').click()
 
     except:
         print("\tcouldnt find the second one")
 
-
-    print(items[count].name + " costing " +items[count].price )
+    print(items[count].name + " costing " + items[count].price)
     count += 1
-
